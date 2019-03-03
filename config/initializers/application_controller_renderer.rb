4a1cfa7e8070e1d358c9fa0c8770cc2fd88b991a
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2019-03-03 19:33:42 +0000

    no message

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -7,8 +7,3 @@
 #   )
 # end
 #
-require 'elasticsearch/model'
-
-if ENV['https://f73agaf2fx:os9mr8w6yl@maple-689483418.eu-west-1.bonsaisearch.net']
-  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['https://f73agaf2fx:os9mr8w6yl@maple-689483418.eu-west-1.bonsaisearch.net'], logs: true})
-end
