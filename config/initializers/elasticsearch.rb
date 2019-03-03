a399d5383e39600de228e9716910f8601c0a6112
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2019-03-03 13:34:32 +0000

    no message

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,6 +1,6 @@
-if  Rails.env.development?
-  url = ENV["https://f73agaf2fx:os9mr8w6yl@maple-689483418.eu-west-1.bonsaisearch.net"]
-  transport_options = { request: { timeout: 250 } }
-  options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
+if Rails.env == 'production'
+  url = ENV["BONSAI_URL"]
+  transport_options = {request: {timeout: 250}}
+  options = {hosts: url, retry_on_failure: true, transport_options: transport_options}
   Searchkick.client = Elasticsearch::Client.new(options)
 end
