25a8308b0a724dac38997826a7455c7af30ac9aa
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2019-03-03 00:14:55 +0000

    no message

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,4 @@
-if Rails.env = 'production'
+if  Rails.env.production?
   url = ENV["https://r6c1h7sz4:b6g15nbk81@olive-544765962.eu-west-1.bonsaisearch.net"]
   transport_options = { request: { timeout: 250 } }
   options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
