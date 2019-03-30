82840befeddc9d596e3154f9f41c8bf8940b7b61
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2019-03-30 13:20:38 +0000

    no message

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -76,9 +76,6 @@ class GamesController < ApplicationController
     end
   end
 
-
-  # private
-
   # Use callbacks to share common setup or constraints between actions.
   def set_game
     @game = Game.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :validatable
   has_many :games
   has_many :reviews, dependent: :destroy
+  validates :email, uniqueness: true
 
 end
